f106ba0b276359310d7ea8ebc9ffc9ea5b41d03c
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2024-10-02 13:24:09 -0300

    Add files via upload

--- social/src/firebase/config.js
+++ social/src/firebase/config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEXc3CanmGtfax4vz3UWOcfoLU4Npffz8",
@@ -7,9 +7,10 @@ const firebaseConfig = {
   projectId: "projsocial-73b68",
   storageBucket: "projsocial-73b68.appspot.com",
   messagingSenderId: "226762492660",
-  appId: "1:226762492660:web:34c89a4c1242a6c21caca1",
+  appId: "1:226762492660:web:34c89a4c1242a6c21caca1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
